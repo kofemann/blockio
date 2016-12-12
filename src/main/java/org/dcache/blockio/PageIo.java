7989910ab2d2c1e98d97e3620e28ba8a35ff81ef
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2016-12-12 23:46:22 +0100

    use StampedLock with HashMap to populate map of pages in use

--- src/main/java/org/dcache/blockio/PageIo.java
+++ src/main/java/org/dcache/blockio/PageIo.java
@@ -1,22 +1,20 @@
 package org.dcache.blockio;
 
-import com.google.common.base.Throwables;
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.RemovalNotification;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.StandardOpenOption;
-import java.util.concurrent.ExecutionException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.locks.StampedLock;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
 public class PageIo {
 
-    private final Cache<Long, Page> cache;
+    private final Map<Long, Page> cache;
 
     private final int pageSize;
     private final FileChannel channel;
@@ -24,7 +22,18 @@ public class PageIo {
     private IOException ioError;
 
     /**
+     * Lock to protect page eviction.
+     */
+    private final StampedLock lock;
+
+    /**
+     * Maximal number of cached pages.
+     */
+    private final int count;
+
+    /**
      * Create new PageIo backed by given {@link FileChannel}.
+     *
      * @param count the total number of cached pages.
      * @param pageSize single page size
      * @param channel {@link FileChannel} used to store the data.
@@ -36,37 +45,60 @@ public class PageIo {
 
         this.pageSize = pageSize;
         this.channel = channel;
+        this.count = count;
 
-        cache = CacheBuilder.newBuilder()
-                .maximumSize(count)
-                .removalListener((RemovalNotification<Long, Page> rn) -> {
-                    try {
-                        Page p = rn.getValue();
-                        p.flush();
-                    } catch (IOException e) {
-                        ioError = e;
-                    }
-                })
-                .build();
+        cache = new HashMap<>();
+        lock = new StampedLock();
     }
 
     private Page getPage(long pageId) throws IOException {
 
+        Page p = null;
+        long sl = lock.readLock();
         try {
-            Page p = cache.get(pageId, () -> {
-                Page pp = new Page(ByteBuffer.allocate(pageSize), pageId * pageSize, channel);
-                pp.load();
-                return pp;
-            });
-             // getting Page from cache may trigger page load/flush and IO error.
-            if (ioError != null) {
-                throw ioError;
+            while(true) {
+                p = cache.get(pageId);
+                if (p != null) {
+                    break;
+                }
+
+                // try to upgrade to a write lock
+                long wl = lock.tryConvertToWriteLock(sl);
+                if (wl == 0) {
+                    // failed to covert to write lock.
+                    lock.unlockRead(sl);
+                    sl = lock.writeLock();
+
+                    // retry the loop - either entry will be in the cache, inserted
+                    // by an other threas, or we get have exclusive lock to insert it.
+                    continue;
+                }
+
+                // lucky thread with write access
+                sl = wl;
+
+                if (cache.size() > count - 1) {
+                    // we need to free a page
+                    // FIXME: we simply kill the first key without wating for release
+                    Optional<Long> oid = cache.keySet()
+                            .stream()
+                            .findAny();
+
+                    if (oid.isPresent()) {
+                        Page evicted = cache.remove(oid.get());
+
+                        // FIXME: wait for ref-count == 0
+                        evicted.flush();
+                    }
+                }
+
+                p = new Page(ByteBuffer.allocate(pageSize), pageId * pageSize, channel);
+                cache.put(pageId, p);
+                p.load();
             }
             return p;
-        } catch (ExecutionException e) {
-            Throwable cause = e.getCause();
-            Throwables.throwIfInstanceOf(cause, IOException.class);
-            throw new IOException(cause);
+        } finally {
+            lock.unlock(sl);
         }
     }
 
@@ -115,9 +147,21 @@ public class PageIo {
     }
 
     public void flushAll() throws IOException {
-        cache.invalidateAll();
-        if (ioError != null) {
-            throw ioError;
+        long stamp = lock.writeLock();
+        try {
+            cache.forEach((k, v) -> {
+                try {
+                    v.flush();
+                } catch (IOException e) {
+                    ioError = e;
+                }
+            });
+
+            if (ioError != null) {
+                throw ioError;
+            }
+        } finally {
+            lock.unlock(stamp);
         }
     }
 
@@ -130,10 +174,10 @@ public class PageIo {
                 StandardOpenOption.WRITE,
                 StandardOpenOption.READ);
 
-        PageIo inPageCache = new PageIo(64 * 1024, 512, in);
-        PageIo outPageCache = new PageIo(64 * 1024, 8192, out);
+        PageIo inPageCache = new PageIo(8192, 64 * 1024, in);
+        PageIo outPageCache = new PageIo(64, 8192, out);
 
-        ByteBuffer b = ByteBuffer.allocate(64);
+        ByteBuffer b = ByteBuffer.allocate(4096);
 
         long offset = 0;
         while (true) {
@@ -145,10 +189,10 @@ public class PageIo {
             b.flip();
             outPageCache.write(offset, b);
             offset += n;
-            System.out.print(new String(b.array(), 0, b.remaining(), StandardCharsets.UTF_8));
+            //  System.out.print(new String(b.array(), 0, b.remaining(), StandardCharsets.UTF_8));
         }
         System.out.println();
-
+        inPageCache.flushAll();
         outPageCache.flushAll();
     }
 }
